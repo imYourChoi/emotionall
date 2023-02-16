f29de91b72f0f108fc813ab2a6caccae5c0df3de
imYourChoi <jeehun@kaist.ac.kr>
2023-02-17 00:21:54 +0900

    Add: redirect to home when unknown path

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -1,8 +1,21 @@
-import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Header from "@/components/layout/Header";
 import Navigation from "@/components/layout/Navigation";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  console.log(router);
+  useEffect(() => {
+    if (
+      !["/", "/rooms", "/chat/[id]", "/contents", "/settings"].includes(
+        router.pathname
+      )
+    ) {
+      router.replace("/");
+    }
+  });
   return (
     <main id="container" className="relative py-[60px]">
       <Header />
