169b79f4401acccdcab098b960ae7bb75cf34bf8
r4bb1t <r4bb1t.github@gmail.com>
2023-02-17 21:54:15 +0900

    home my profile dummy data

--- front/src/components/home/MyProfile.jsx
+++ front/src/components/home/MyProfile.jsx
@@ -1,6 +1,9 @@
 import { useEmotion } from "@/contexts/emotionContext";
 import { useUser } from "@/contexts/userContext";
 import Avatar from "../avatar/Avatar";
+import { badge } from "@/constants/badge";
+import cc from "classcat";
+import { EditIcon } from "../icons/Icons";
 
 const MyProfile = () => {
   const { user } = useUser();
@@ -9,12 +12,27 @@ const MyProfile = () => {
     <div className="w-full flex items-center px-8 pt-8 pb-4">
       <Avatar width="128px" avatar={user.avatar} emotion={emotion} />
       <div>
-        <div className="text-positive font-semibold">파워긍정 수다쟁이</div>
-        <div className="font-extrabold text-lg pb-2">
-          {user.nickname || "test"}
+        <div
+          className={cc([
+            "font-semibold",
+            badge[user.badge].color === "positive" && "text-positive",
+            badge[user.badge].color === "negative" && "text-negative",
+            badge[user.badge].color === "neutral" && "text-neutral",
+            badge[user.badge].color === "ambiguous" && "text-ambiguous",
+          ])}
+        >
+          {badge[user.badge].name}
         </div>
-        <div className="px-4 py-2 font-semibold rounded-2xl rounded-tl-none bg-black-100">
-          인생이란 멀까........
+        <div className="font-extrabold text-lg pb-2 flex items-center gap-2">
+          {user.nickname || "test"} <EditIcon />
+        </div>
+        <div className="px-4 py-2 font-semibold rounded-2xl rounded-tl-none bg-black-100 flex items-center gap-2 text-sm">
+          {user.message || (
+            <span className="text-black-400">
+              설정된 상태 메세지가 없습니다.
+            </span>
+          )}
+          <EditIcon />
         </div>
       </div>
     </div>
---
+++ front/src/constants/badge.js
@@ -0,0 +1,15 @@
+export const badge = {
+  "": {
+    name: "-",
+    color: "neutral",
+    description: [],
+  },
+  "positive-max": {
+    name: "파워긍정 수다쟁이",
+    color: "positive",
+    description: [
+      "전체 말풍선 중 긍정적인 말풍선 비율이 30% 이상이에요.",
+      "전체 말풍선 개수가 상위 20% 이내에요.",
+    ],
+  },
+};
