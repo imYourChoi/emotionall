fc22b052e27d66d6a205222c55f370737a058cd6
imYourChoi <jeehun@kaist.ac.kr>
2023-02-17 23:39:55 +0900

    Add: style props to Avatar

--- front/src/components/avatar/Avatar.jsx
+++ front/src/components/avatar/Avatar.jsx
@@ -1,6 +1,6 @@
-const Avatar = ({ width, avatar, emotion }) => {
+const Avatar = ({ width, avatar, emotion, style }) => {
   return (
-    <div className="w-12 aspect-square relative" style={{ width }}>
+    <div className="w-12 aspect-square relative" style={{ width, ...style }}>
       <img
         className="absolute"
         src={`/assets/avatar/skin/${avatar.skin}.png`}
