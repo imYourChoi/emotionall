2cddf9be83a06307b9b638c193f1dba2c99122b8
imYourChoi <jeehun@kaist.ac.kr>
2023-02-17 23:28:18 +0900

    Add: isMyself and isLast to message set

--- front/src/components/chat/Message.jsx
+++ front/src/components/chat/Message.jsx
@@ -1,7 +1,24 @@
-const Message = ({ chat }) => {
+const Message = ({ chat, isMyself, isLast }) => {
+  const getRadius = () => {
+    if (!isLast) return " rounded-2xl";
+    return (
+      " rounded-t-2xl" + (isMyself ? " rounded-bl-2xl" : " rounded-br-2xl")
+    );
+  };
+  const getColor = () => {
+    return " bg-neutral-blend";
+  };
   return (
-    <div className="w-full py-[4.5px]">
-      <div className="w-fit px-[18px] py-2.5 rounded-2xl bg-neutral-blend text-base font-medium">
+    <div
+      className={"w-full py-[4.5px] flex" + (isMyself ? " justify-end" : "")}
+    >
+      <div
+        className={
+          "w-fit px-[18px] py-2.5 text-base font-medium" +
+          getColor() +
+          getRadius()
+        }
+      >
         {chat.text}
       </div>
     </div>
--- front/src/components/chat/MessageSet.jsx
+++ front/src/components/chat/MessageSet.jsx
@@ -1,11 +1,17 @@
 import Message from "./Message";
 
 const MessageSet = ({ chats, user_id }) => {
-  const isMyself = chats[0].user_id === user_id;
+  //   const isMyself = chats[0].user_id === user_id;
+  const isMyself = Math.random() > 0.5;
   return (
     <div className="pt-16">
       {chats?.map((chat, idx) => (
-        <Message key={idx} chat={chat} />
+        <Message
+          key={idx}
+          chat={chat}
+          isMyself={isMyself}
+          isLast={idx === chats.length - 1}
+        />
       ))}
     </div>
   );
