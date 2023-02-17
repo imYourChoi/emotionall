18e2dd3a2f207db6446b5d9fe10bb81d5b6904e7
imYourChoi <jeehun@kaist.ac.kr>
2023-02-18 00:38:46 +0900

    Add: avatar to room list

--- front/src/components/chat/RoomItem.jsx
+++ front/src/components/chat/RoomItem.jsx
@@ -1,15 +1,31 @@
+import Avatar from "../avatar/Avatar";
+
 const RoomItem = ({ friend, onClick }) => {
+  const getEmotion = () => {
+    return "bg-positive-blend";
+  };
   return (
     <div
       onClick={onClick}
       className="relative w-full h-[72px] px-6 py-1 flex gap-x-2.5"
     >
-      <img className="w-16 h-16 bg-black-200 rounded-full" alt="profile-img" />
+      <Avatar
+        width="64px"
+        avatar={{
+          skin: Math.floor(Math.random() * 4),
+          eyes: Math.floor(Math.random() * 5),
+          hair: Math.floor(Math.random() * 15),
+          glasses: Math.floor(Math.random() * 4),
+        }}
+        emotion="positive"
+      />
       <div className="flex flex-col justify-center gap-y-1">
         <div className="text-sm font-bold">{friend.name}</div>
         <div className="text-base font-medium">{friend.message}</div>
       </div>
-      <div className="absolute right-0 inset-y-0 bg-positive-blend h-full w-3" />
+      <div
+        className={"absolute right-0 inset-y-0 h-full w-3 " + getEmotion()}
+      />
     </div>
   );
 };
