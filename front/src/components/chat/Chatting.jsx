cd7b9fec5fba9c685e4e923b5e7afcb7a52f0e7a
imYourChoi <jeehun@kaist.ac.kr>
2023-02-18 03:24:38 +0900

    Working on message receiving

--- front/src/components/chat/Chatting.jsx
+++ front/src/components/chat/Chatting.jsx
@@ -1,27 +1,33 @@
 import { useRef, useState, useEffect, useCallback } from "react";
+import { useRouter } from "next/router";
 import { io } from "socket.io-client";
 import useTextAreaAutosize from "@/hooks/useTextareaAutosize";
 import MessageForm from "./MessageForm";
 import MessageBody from "./MessageBody";
 
-// const socket = io("http://192.168.8.85:80/chat");
-const socket = io("http://localhost:80");
+const socket = io("http://192.168.8.85:80/chat");
+// const socket = io("http://localhost:80");
 
 const Chatting = () => {
+  const router = useRouter();
   const textAreaRef = useRef(null);
   const messageBodyRef = useRef(null);
 
   const [chats, setChats] = useState([]);
   const [message, setMessage] = useState("");
   const [disabled, setDisabled] = useState(!message);
+  const room_id = router.query.id;
   useTextAreaAutosize(textAreaRef.current, message);
 
   useEffect(() => {
     const handleReceiveMessage = (chat) => {
+      console.log(chat);
       setChats((prevChats) => [...prevChats, chat]);
     };
 
-    socket.on("message", handleReceiveMessage);
+    socket.on("message", handleReceiveMessage, (response) => {
+      console.log(response);
+    });
     return () => {
       socket.off("message", handleReceiveMessage);
     };
@@ -33,9 +39,12 @@ const Chatting = () => {
 
   const handleSendMessage = useCallback(() => {
     socket.emit("message", {
-      member_id: "my_id",
-      text: message,
-      time: new Date().toISOString(),
+      room_id: room_id,
+      message: {
+        member_id: 4008,
+        text: message,
+        time: new Date().toISOString(),
+      },
     });
     setMessage("");
     setDisabled(true);
--- front/src/components/chat/SearchItem.jsx
+++ front/src/components/chat/SearchItem.jsx
@@ -3,23 +3,22 @@ import { AddIcon } from "../icons/Icons";
 
 const SearchItem = ({ friend, onClick }) => {
   return (
-    <div
-      onClick={onClick}
-      className="relative w-full h-[72px] pl-6 pr-4 py-1 flex items-center justify-between gap-x-2.5"
-    >
-      <Avatar
-        width="48px"
-        avatar={{
-          skin: Math.floor(Math.random() * 4),
-          eyes: Math.floor(Math.random() * 5),
-          hair: Math.floor(Math.random() * 15),
-          glasses: Math.floor(Math.random() * 4),
-        }}
-        emotion="positive"
-        style={{ flex: "0 0 48px" }}
-      />
-      <div className="text-base font-mediu flex-initial">{friend.name}</div>
-      <div className="flex-auto flex justify-end">
+    <div className="relative w-full h-[72px] pl-6 pr-4 py-1 flex items-center justify-between gap-x-2.5">
+      <div className="flex items-center justify-between gap-x-2.5">
+        <Avatar
+          width="48px"
+          avatar={{
+            skin: Math.floor(Math.random() * 4),
+            eyes: Math.floor(Math.random() * 5),
+            hair: Math.floor(Math.random() * 15),
+            glasses: Math.floor(Math.random() * 4),
+          }}
+          emotion="positive"
+          style={{ flex: "0 0 48px" }}
+        />
+        <div className="text-base font-mediu flex-initial">{friend.name}</div>
+      </div>
+      <div onClick={onClick}>
         <AddIcon className="cursor-pointer" />
       </div>
     </div>
